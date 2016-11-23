25e1c412c80bacc3e1fd52ae1f84518b8098594b
Maxime Signoret <maxime.signoret@gmail.com>
2016-11-23 22:14:11 +0100

    [master] feat: add comments to movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
   validates :url, presence: true, length: { minimum: 5 }
 end
